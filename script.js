784eb243789aad124c1a483f8170c4bd0c497873
DOMINUS <vadimisenko953@gmail.com>
2026-04-29 10:56:05 +0300

    video section and adaptive is done

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const menu = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', () => {
 menu.classList.toggle('active');
 });
+
+
+const playBtn = document.querySelector('.video__play');
+const frame = document.querySelector('.video__frame');
+
+playBtn.addEventListener('click', () => {
+    frame.src="https://www.youtube.com/embed/4nfq18MG7Mo?autoplay=1";
+    frame.style.display = "block";
+    playBtn.style.display = "none";
+})
